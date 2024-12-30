2e9ac88b67d6f1ccaeabb256b1d2d8405485b1b4
dimascodes <dimasarisp53@gmail.com>
2024-12-30 14:01:47 +0700

    next.js

---
+++ diabetes-prediction/pages/index.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import styles from "../public/styles.css";
+
+export default function Home() {
+  const [formData, setFormData] = useState({
+    Pregnancies: "",
+    Glucose: "",
+    BloodPressure: "",
+    SkinThickness: "",
+    Insulin: "",
+    BMI: "",
+    DiabetesPedigreeFunction: "",
+    Age: "",
+  });
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setResult("");
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        body: new URLSearchParams(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setResult(data.prediction);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Diabetes Prediction</h1>
+      <form onSubmit={handleSubmit}>
+        {Object.keys(formData).map((field) => (
+          <input
+            key={field}
+            type="number"
+            name={field}
+            placeholder={field}
+            step="any"
+            value={formData[field]}
+            onChange={handleChange}
+            required
+          />
+        ))}
+        <button type="submit">Prediksi</button>
+      </form>
+      {result && (
+        <div id="result">
+          <h2>Prediction: {result}</h2>
+        </div>
+      )}
+      {error && (
+        <div id="error">
+          <h2>Error: {error}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
